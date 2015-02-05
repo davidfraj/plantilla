a6a7aa2e46a86574d17a742edb07b6db187e357c
davidfraj <davidfraj@gmail.com>
2015-02-05 19:27:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>plantilla</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	
+	<section id="wrapper">
+		<header>
+			ENCABEZADO
+		</header>
+		<section id="central">
+			<div id="contenido">
+				CONTENIDO
+			</div>
+			<nav>
+				MENU
+			</nav>
+		</section>
+		<footer>
+			PIE
+		</footer>
+	</section>
+</body>
+</html>
